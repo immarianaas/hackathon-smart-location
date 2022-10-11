3788a00e76494ac6aa2bcf8831ef9db303e6d835
Mariana Santos <im.marianaas@gmail.com>
2022-10-11 11:26:40 +0100

    allow getting various types of objects

--- smart-location-interface/src/app/entity.service.ts
+++ smart-location-interface/src/app/entity.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { forkJoin, merge, mergeWith, Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BeachEntity } from './beach-entity';
+import { Entity, EntityType } from './entity';
+import { EMPTY } from 'rxjs'
 
 
 @Injectable({
@@ -14,7 +16,33 @@ export class EntityService {
     private http: HttpClient
   ) { }
 
-  getEntities(): Observable<BeachEntity[]> {
-    return this.http.get<BeachEntity[]>( this.url );
+  private getBeachEntities(): Observable<BeachEntity[]> {
+    return this.http.get<BeachEntity[]>( this.url + "?type=Beach");
+  }
+
+  // Exemplo apenas; apagar
+  private getBeachEntities2(): Observable<Entity[]> {
+    return this.http.get<BeachEntity[]>( this.url + "?type=Beach");
+  }
+  
+  getEntities( type : EntityType ): Observable<BeachEntity[] | Entity[]> {
+    switch( type )
+    {
+      case EntityType.BEACH:
+        return this.getBeachEntities();
+
+      default:
+        return EMPTY;
+    }
+  }
+
+  getAllEntities(): Observable<BeachEntity[] | Entity[]>
+  {
+    var a = this.getBeachEntities() ;
+
+    // aqui podemos juntar pedidos de vários tipos diferentes;
+    // isto é apenas um exemplo
+    var b = this.getBeachEntities2() ;    
+    return merge( a, b );
   }
 }
--- smart-location-interface/src/app/map/map.component.ts
+++ smart-location-interface/src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EntityService } from '../entity.service';
 import { BeachEntity } from '../beach-entity';
+import { EntityType } from '../entity';
 
 @Component({
   selector: 'app-map',
@@ -8,6 +9,13 @@ import { BeachEntity } from '../beach-entity';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+
+  /*
+   * NOTA: eu meti a logica de ir buscar dados aqui
+   * mas foi só como exemplo, temos de discutir como
+   * vamos organizar os componentes e definir qual fica 
+   * com essa responsabilidade
+   */
 
   beaches: BeachEntity[];
 
@@ -18,10 +26,25 @@ export class MapComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.entityService.getEntities().subscribe({
+    this.getAllEntities();
+  }
+
+  private getAllEntities(): void {
+    // aqui vamos conseguir mapear qualquer tipo que "vier" para
+    // o array com o tipo correto
+    this.entityService.getAllEntities().subscribe({
       next: (entities) => { 
-        entities.forEach( e => this.beaches.push(e) );
-        console.log("entities.length= ", entities.length)
+        entities.forEach( e => {
+          // verify 'entities' type
+          switch( e.type )
+          {
+            case "Beach": // TODO change to enum or smth
+              this.beaches.push( <BeachEntity> e );
+              break;
+          }
+          
+        });
+        
       },
       error: (e) => {
         console.log(e);
