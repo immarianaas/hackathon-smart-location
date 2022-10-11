4113197f94df86949f7a8b300f3fc7c5c59325d1
Mariana Santos <im.marianaas@gmail.com>
2022-10-11 11:26:05 +0100

    add interface inheritance

--- smart-location-interface/src/app/beach-entity.ts
+++ smart-location-interface/src/app/beach-entity.ts
@@ -1,6 +1,6 @@
-export interface BeachEntity {
-    id: string;
-    type: string;
+import { Entity } from "./entity";
+
+export interface BeachEntity extends Entity {
     name: {
         value: string;
     }
